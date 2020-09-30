c22226bcf757a0ede4e190590f0986cfa36050cf
tylerjrichards <tylerjrichards@gmail.com>
2020-09-29 20:33:26 -0400

    first app push

---
+++ pizza.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.text('hey')
+
+
+df_pizza = pd.read_csv('https://raw.githubusercontent.com/tylerjrichards/Barstool_Pizza/master/pizza_data.csv')
