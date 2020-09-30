79566d3c9131b567574ea0eda80194d6f230e163
tylerjrichards <tylerjrichards@gmail.com>
2020-09-29 20:39:03 -0400

    added first app

--- pizza.py
+++ pizza.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import seaborn as sns
 
-st.text('hey')
-
+st.markdown('Hey there, this is just a test for the new streamlit deployment method')
 
 df_pizza = pd.read_csv('https://raw.githubusercontent.com/tylerjrichards/Barstool_Pizza/master/pizza_data.csv')
+
+
+sns.distplot(df_pizza['dave_score_int'])
+plt.xlabel('Dave Score')
+plt.ylabel('Proportion')
+plt.title('Dave Pizza Score')
+st.pyplot()
